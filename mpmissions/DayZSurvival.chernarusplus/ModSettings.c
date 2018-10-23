a5b57c0df5ac15d38998ab4814a69fc0c82ff9c4
Da0ne <38754008+Da0ne@users.noreply.github.com>
2018-10-24 04:01:34 +1030

    Add files via upload

---
+++ mpmissions/DayZSurvival.chernarusplus/ModSettings.c
@@ -0,0 +1,5 @@
+m_VanillaLoadouts = true; //Set to true to Spawn Players with custom loadouts
+m_SpawnArmed 	  = true; //Spawn fresh spawns with a pistol, weapon types can be changed in DayZSurvival.c
+m_StaminaStatus   = true; // set to true to disable stamina
+m_SafeZone	      = true; // Safe Zone trigger
+m_CustomBuildings = true; //Spawns the buildings you add in file BuildingSpawner.c
