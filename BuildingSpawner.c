0869e85b1da1ed0067767ce0c29fd9cc96ba6ebe
Da0ne <38754008+Da0ne@users.noreply.github.com>
2018-10-24 20:50:11 +1030

    Add files via upload

---
+++ BuildingSpawner.c
@@ -0,0 +1,165 @@
+void BuildingSpawner()
+{
+		EntityAI tmp;
+		tmp = GetGame().CreateObject( "Land_Airfield_Hangar_Green", "11990.633789 146.099136 12485.503906", false, true );
+		tmp.SetOrientation("-157.000046 -1.000000 -0.000000");
+		tmp.SetDirection(tmp.GetDirection());
+		tmp = GetGame().CreateObject( "Land_Airfield_Hangar_Green", "12033.440430 146.253677 12469.438477", false, true );
+		tmp.SetOrientation("-157.000031 0.000000 -0.000000");
+		tmp.SetDirection(tmp.GetDirection());
+		tmp = GetGame().CreateObject( "Land_Airfield_Hangar_Green", "12076.359375 146.405930 12451.586914", false, true );
+		tmp.SetOrientation("-157.000015 0.000000 -0.000000");
+		tmp.SetDirection(tmp.GetDirection());
+		tmp = GetGame().CreateObject( "Land_Tisy_HQ", "12122.756836 143.341339 12437.160156", false, true );
+		tmp.SetOrientation("-65.000000 0.000000 -0.000000");
+		tmp.SetDirection(tmp.GetDirection());
+		tmp = GetGame().CreateObject( "Land_Mil_Fortified_Nest_Watchtower", "12111.398438 142.148178 12464.422852", false, true );
+		tmp.SetOrientation("-133.999954 0.000000 -0.000000");
+		tmp.SetDirection(tmp.GetDirection());
+		tmp = GetGame().CreateObject( "Land_Container_1Moh", "12153.124023 141.149719 12634.041016", false, true );
+		tmp.SetOrientation("0.000000 0.000000 -0.000000");
+		tmp.SetDirection(tmp.GetDirection());
+		tmp = GetGame().CreateObject( "Land_Container_1Mo", "12153.124023 143.700699 12634.194336", false, true );
+		tmp.SetOrientation("-22.999998 0.000000 -0.000000");
+		tmp.SetDirection(tmp.GetDirection());
+		tmp = GetGame().CreateObject( "Land_Container_1Aoh", "12159.783203 141.099960 12627.764648", false, true );
+		tmp.SetOrientation("-55.999992 0.000000 -0.000000");
+		tmp.SetDirection(tmp.GetDirection());
+		tmp = GetGame().CreateObject( "Land_Container_1Bo", "12159.695313 143.650513 12628.934570", false, true );
+		tmp.SetOrientation("0.000000 0.000000 -0.000000");
+		tmp.SetDirection(tmp.GetDirection());
+		tmp = GetGame().CreateObject( "Land_Container_1Bo", "12159.755859 141.100082 12630.863281", false, true );
+		tmp.SetOrientation("-66.000000 0.000000 -0.000000");
+		tmp.SetDirection(tmp.GetDirection());
+		tmp = GetGame().CreateObject( "Land_Tisy_RadarB_Base", "12142.295898 140.014191 12655.396484", false, true );
+		tmp.SetOrientation("-68.000000 0.000000 -0.000000");
+		tmp.SetDirection(tmp.GetDirection());
+		tmp = GetGame().CreateObject( "Land_Mil_Barracks6", "12241.260742 140.009491 12587.833984", false, true );
+		tmp.SetOrientation("47.000031 2.000001 -0.000000");
+		tmp.SetDirection(tmp.GetDirection());
+		tmp = GetGame().CreateObject( "Land_Mil_Barracks5", "12282.513672 145.910843 12540.527344", false, true );
+		tmp.SetOrientation("-131.000107 0.000000 -0.000000");
+		tmp.SetDirection(tmp.GetDirection());
+		tmp = GetGame().CreateObject( "Land_Mil_Barracks4", "12386.727539 141.500793 12526.896484", false, true );
+		tmp.SetOrientation("-32.999985 0.000000 -0.000000");
+		tmp.SetDirection(tmp.GetDirection());
+		tmp = GetGame().CreateObject( "Land_Mil_Barracks4", "12378.078125 141.505005 12541.388672", false, true );
+		tmp.SetOrientation("-30.000004 0.000000 -0.000000");
+		tmp.SetDirection(tmp.GetDirection());
+		tmp = GetGame().CreateObject( "Land_Mil_Barracks3", "12355.725586 142.450974 12578.972656", false, true );
+		tmp.SetOrientation("-120.999969 0.000000 -0.000000");
+		tmp.SetDirection(tmp.GetDirection());
+		tmp = GetGame().CreateObject( "Land_Wreck_Uaz", "12373.697266 140.750443 12551.580078", false, true );
+		tmp.SetOrientation("-105.000000 0.000000 -0.000000");
+		tmp.SetDirection(tmp.GetDirection());
+		tmp = GetGame().CreateObject( "Land_Wreck_V3S", "12362.463867 141.250046 12555.341797", false, true );
+		tmp.SetOrientation("-142.999985 0.000000 -0.000000");
+		tmp.SetDirection(tmp.GetDirection());
+		tmp = GetGame().CreateObject( "Land_Wreck_Volha_Blue", "12367.454102 140.649948 12567.520508", false, true );
+		tmp.SetOrientation("0.000000 0.000000 -0.000000");
+		tmp.SetDirection(tmp.GetDirection());
+		tmp = GetGame().CreateObject( "Land_Construction_Crane", "12317.819336 158.452194 12351.926758", false, true );
+		tmp.SetOrientation("-40.000004 0.000000 -0.000000");
+		tmp.SetDirection(tmp.GetDirection());
+		tmp = GetGame().CreateObject( "Land_Construction_Building", "12353.462891 146.695618 12353.335938", false, true );
+		tmp.SetOrientation("12.000003 0.000000 -0.000000");
+		tmp.SetDirection(tmp.GetDirection());
+		tmp = GetGame().CreateObject( "Land_Container_1Bo", "12358.243164 153.300247 12358.978516", false, true );
+		tmp.SetOrientation("0.000000 0.000000 -0.000000");
+		tmp.SetDirection(tmp.GetDirection());
+		tmp = GetGame().CreateObject( "Land_Wreck_C130J", "12423.063477 144.933746 12400.800781", false, true );
+		tmp.SetOrientation("-87.000008 -1.000000 13.000000");
+		tmp.SetDirection(tmp.GetDirection());
+		tmp = GetGame().CreateObject( "Land_Wreck_C130J", "11380.672852 191.271561 12864.492188", false, true );
+		tmp.SetOrientation("121.999931 -1.999996 21.999998");
+		tmp = GetGame().CreateObject( "Land_City_FireStation", "12070.803711 149.259705 12566.105469", false, true );
+		tmp.SetOrientation("22.000023 0.000000 -0.000000");
+		tmp.SetDirection(tmp.GetDirection());
+		tmp = GetGame().CreateObject( "Land_Mil_ATC_Big", "12360.564453 155.447235 12468.625977", false, true );
+		tmp.SetOrientation("-37.999989 0.000000 -0.000000");
+		tmp.SetDirection(tmp.GetDirection());
+		tmp = GetGame().CreateObject( "Land_Mil_Barracks1", "11902.087891 142.248169 12544.751953", false, true );
+		tmp.SetOrientation("-64.000015 0.000000 -0.000000");
+		tmp.SetDirection(tmp.GetDirection());
+		tmp = GetGame().CreateObject( "Land_Mil_Barracks2", "11860.837891 141.265808 12562.627930", false, true );
+		tmp.SetOrientation("-60.000011 1.000000 -0.000000");
+		tmp.SetDirection(tmp.GetDirection());
+		tmp = GetGame().CreateObject( "Land_Mil_Barracks3", "11842.736328 142.303757 12570.661133", false, true );
+		tmp.SetOrientation("-63.000011 0.000000 -0.000000");
+		tmp.SetDirection(tmp.GetDirection());
+		tmp = GetGame().CreateObject( "Land_Mil_ReinforcedTank2", "11811.361328 140.124710 12430.944336", false, true );
+		tmp.SetOrientation("0.000000 0.000000 -0.000000");
+		tmp.SetDirection(tmp.GetDirection());
+		tmp = GetGame().CreateObject( "Land_Mil_ReinforcedTank2", "11912.859375 140.560303 12413.392578", false, true );
+		tmp.SetOrientation("0.000000 0.000000 -0.000000");
+		tmp.SetDirection(tmp.GetDirection());
+		tmp = GetGame().CreateObject( "Land_Mil_ReinforcedTank1", "12506.320313 139.874039 12450.527344", false, true );
+		tmp.SetOrientation("112.999969 0.000000 -0.000000");
+		tmp.SetDirection(tmp.GetDirection());
+		tmp = GetGame().CreateObject( "Land_Mil_ReinforcedTank1", "12502.806641 140.085983 12476.512695", false, true );
+		tmp.SetOrientation("114.999969 -1.000000 -0.000000");
+		tmp.SetDirection(tmp.GetDirection());
+		tmp = GetGame().CreateObject( "Land_Tisy_Barracks", "12381.936523 144.301437 12589.898438", false, true );
+		tmp.SetOrientation("58.999989 0.000000 -0.000000");
+		tmp.SetDirection(tmp.GetDirection());
+		tmp = GetGame().CreateObject( "Land_Tisy_Garages", "12585.142578 164.141495 12644.763672", false, true );
+		tmp.SetOrientation("66.999985 4.000006 1.000000");
+		tmp.SetDirection(tmp.GetDirection());
+		tmp = GetGame().CreateObject( "Land_Wreck_Uaz", "12577.993164 162.855591 12638.497070", false, true );
+		tmp.SetOrientation("38.000011 14.000003 -12.999999");
+		tmp.SetDirection(tmp.GetDirection());
+		tmp = GetGame().CreateObject( "Land_Sawmill_Building", "12171.803711 146.016998 12846.818359", false, true );
+		tmp.SetOrientation("16.000004 0.000000 -0.000000");
+		tmp.SetDirection(tmp.GetDirection());
+		tmp = GetGame().CreateObject( "Land_Smokestack_Medium", "12185.951172 139.999863 12845.899414", false, true );
+		tmp.SetOrientation("57.999996 0.000000 -0.000000");
+		tmp.SetDirection(tmp.GetDirection());
+}
+
+ref TStringArray ZombieClasses = {
+		
+		"ZmbM_HermitSkinny_Base","ZmbM_HermitSkinny_Beige","ZmbM_HermitSkinny_Black","ZmbM_HermitSkinny_Green",
+		"ZmbM_HermitSkinny_Red","ZmbM_FarmerFat_Base","ZmbM_FarmerFat_Beige","ZmbM_FarmerFat_Blue","ZmbM_FarmerFat_Brown",
+		"ZmbM_FarmerFat_Green","ZmbF_CitizenANormal_Base","ZmbF_CitizenANormal_Beige","ZmbF_CitizenANormal_Brown",
+		"ZmbF_CitizenANormal_Blue","ZmbM_CitizenASkinny_Base","ZmbM_CitizenASkinny_Blue","ZmbM_CitizenASkinny_Brown",
+		"ZmbM_CitizenASkinny_Grey","ZmbM_CitizenASkinny_Red","ZmbM_CitizenBFat_Base","ZmbM_CitizenBFat_Blue","ZmbM_CitizenBFat_Red",
+		"ZmbM_CitizenBFat_Green","ZmbF_CitizenBSkinny_Base","ZmbF_CitizenBSkinny","ZmbM_PrisonerSkinny_Base","ZmbM_PrisonerSkinny",
+		"ZmbM_FirefighterNormal_Base","ZmbM_FirefighterNormal","ZmbM_FishermanOld_Base","ZmbM_FishermanOld_Blue","ZmbM_FishermanOld_Green",
+		"ZmbM_FishermanOld_Grey","ZmbM_FishermanOld_Red","ZmbM_JournalistSkinny_Base","ZmbM_JournalistSkinny","ZmbF_JournalistNormal_Base",
+		"ZmbF_JournalistNormal_Blue","ZmbF_JournalistNormal_Green","ZmbF_JournalistNormal_Red","ZmbF_JournalistNormal_White",
+		"ZmbM_ParamedicNormal_Base","ZmbM_ParamedicNormal_Blue","ZmbM_ParamedicNormal_Green","ZmbM_ParamedicNormal_Red",
+		"ZmbM_ParamedicNormal_Black","ZmbF_ParamedicNormal_Base","ZmbF_ParamedicNormal_Blue","ZmbF_ParamedicNormal_Green",
+		"ZmbF_ParamedicNormal_Red","ZmbM_HikerSkinny_Base","ZmbM_HikerSkinny_Blue","ZmbM_HikerSkinny_Green","ZmbM_HikerSkinny_Yellow",
+		"ZmbF_HikerSkinny_Base","ZmbF_HikerSkinny_Blue","ZmbF_HikerSkinny_Grey","ZmbF_HikerSkinny_Green","ZmbF_HikerSkinny_Red",
+		"ZmbM_HunterOld_Base","ZmbM_HunterOld_Autumn","ZmbM_HunterOld_Spring","ZmbM_HunterOld_Summer","ZmbM_HunterOld_Winter",
+		"ZmbF_SurvivorNormal_Base","ZmbF_SurvivorNormal_Blue","ZmbF_SurvivorNormal_Orange","ZmbF_SurvivorNormal_Red",
+		"ZmbF_SurvivorNormal_White","ZmbM_SurvivorDean_Base","ZmbM_SurvivorDean_Black","ZmbM_SurvivorDean_Blue","ZmbM_SurvivorDean_Grey",
+		"ZmbM_PolicemanFat_Base","ZmbM_PolicemanFat","ZmbF_PoliceWomanNormal_Base","ZmbF_PoliceWomanNormal","ZmbM_PolicemanSpecForce_Base",
+		"ZmbM_PolicemanSpecForce","ZmbM_SoldierNormal_Base","ZmbM_SoldierNormal","ZmbM_usSoldier_normal_Base",
+		"ZmbM_usSoldier_normal_Woodland","ZmbM_usSoldier_normal_Desert","ZmbM_CommercialPilotOld_Base","ZmbM_CommercialPilotOld_Blue",
+		"ZmbM_CommercialPilotOld_Olive","ZmbM_CommercialPilotOld_Brown","ZmbM_CommercialPilotOld_Grey","ZmbM_PatrolNormal_Base",
+		"ZmbM_PatrolNormal_PautRev","ZmbM_PatrolNormal_Autumn","ZmbM_PatrolNormal_Flat","ZmbM_PatrolNormal_Summer","ZmbM_JoggerSkinny_Base",
+		"ZmbM_JoggerSkinny_Blue","ZmbM_JoggerSkinny_Green","ZmbM_JoggerSkinny_Red","ZmbF_JoggerSkinny_Base","ZmbF_JoggerSkinny_Blue",
+		"ZmbF_JoggerSkinny_Brown","ZmbF_JoggerSkinny_Green","ZmbF_JoggerSkinny_Red","ZmbM_MotobikerFat_Base","ZmbM_MotobikerFat_Beige",
+		"ZmbM_MotobikerFat_Black","ZmbM_MotobikerFat_Blue","ZmbM_VillagerOld_Base","ZmbM_VillagerOld_Blue","ZmbM_VillagerOld_Green",
+		"ZmbM_VillagerOld_White","ZmbM_SkaterYoung_Base","ZmbM_SkaterYoung_Blue","ZmbM_SkaterYoung_Brown","ZmbM_SkaterYoung_Green",
+		"ZmbM_SkaterYoung_Grey","ZmbF_SkaterYoung_Base","ZmbF_SkaterYoung_Brown","ZmbF_SkaterYoung_Striped","ZmbF_SkaterYoung_Violet",
+		"ZmbF_DoctorSkinny_Base","ZmbF_DoctorSkinny","ZmbF_BlueCollarFat_Base","ZmbF_BlueCollarFat_Blue","ZmbF_BlueCollarFat_Green",
+		"ZmbF_BlueCollarFat_Red","ZmbF_BlueCollarFat_White","ZmbF_MechanicNormal_Base","ZmbF_MechanicNormal_Beige","ZmbF_MechanicNormal_Green",
+		"ZmbF_MechanicNormal_Grey","ZmbF_MechanicNormal_Orange","ZmbM_MechanicSkinny_Base","ZmbM_MechanicSkinny_Blue","ZmbM_MechanicSkinny_Grey",
+		"ZmbM_MechanicSkinny_Green","ZmbM_MechanicSkinny_Red","ZmbM_ConstrWorkerNormal_Base","ZmbM_ConstrWorkerNormal_Beige",
+		"ZmbM_ConstrWorkerNormal_Black","ZmbM_ConstrWorkerNormal_Green","ZmbM_ConstrWorkerNormal_Grey","ZmbM_HeavyIndustryWorker_Base",
+		"ZmbM_HeavyIndustryWorker","ZmbM_OffshoreWorker_Base","ZmbM_OffshoreWorker_Green","ZmbM_OffshoreWorker_Orange","ZmbM_OffshoreWorker_Red",
+		"ZmbM_OffshoreWorker_Yellow","ZmbF_NurseFat_Base","ZmbF_NurseFat","ZmbM_HandymanNormal_Base","ZmbM_HandymanNormal_Beige",
+		"ZmbM_HandymanNormal_Blue","ZmbM_HandymanNormal_Green","ZmbM_HandymanNormal_Grey","ZmbM_HandymanNormal_White","ZmbM_DoctorFat_Base",
+		"ZmbM_DoctorFat","ZmbM_Jacket_Base","ZmbM_Jacket_beige","ZmbM_Jacket_black","ZmbM_Jacket_blue","ZmbM_Jacket_bluechecks",
+		"ZmbM_Jacket_brown","ZmbM_Jacket_greenchecks","ZmbM_Jacket_grey","ZmbM_Jacket_khaki","ZmbM_Jacket_magenta","ZmbM_Jacket_stripes",
+		"ZmbF_PatientOld_Base","ZmbF_PatientOld","ZmbM_PatientSkinny_Base","ZmbM_PatientSkinny","ZmbF_ShortSkirt_Base","ZmbF_ShortSkirt_beige",
+		"ZmbF_ShortSkirt_black","ZmbF_ShortSkirt_brown","ZmbF_ShortSkirt_green","ZmbF_ShortSkirt_grey","ZmbF_ShortSkirt_checks",
+		"ZmbF_ShortSkirt_red","ZmbF_ShortSkirt_stripes","ZmbF_ShortSkirt_white","ZmbF_ShortSkirt_yellow","ZmbF_VillagerOld_Base",
+		"ZmbF_VillagerOld_Blue","ZmbF_VillagerOld_Green","ZmbF_VillagerOld_Red","ZmbF_VillagerOld_White","ZmbM_Soldier","ZmbM_SoldierAlice",
+		"ZmbM_SoldierHelmet","ZmbM_SoldierVest","ZmbM_SoldierAliceHelmet","ZmbM_SoldierVestHelmet","ZmbF_MilkMaidOld_Base",
+		"ZmbF_MilkMaidOld_Beige","ZmbF_MilkMaidOld_Black","ZmbF_MilkMaidOld_Green","ZmbF_MilkMaidOld_Grey","ZmbM_priestPopSkinny_Base",
+		"ZmbM_priestPopSkinny","ZmbM_ClerkFat_Base","ZmbM_ClerkFat_Brown","ZmbM_ClerkFat_Grey","ZmbM_ClerkFat_Khaki","ZmbM_ClerkFat_White",
+		"ZmbF_Clerk_Normal_Base","ZmbF_Clerk_Normal_Blue","ZmbF_Clerk_Normal_White","ZmbF_Clerk_Normal_Green","ZmbF_Clerk_Normal_Red",
+};
