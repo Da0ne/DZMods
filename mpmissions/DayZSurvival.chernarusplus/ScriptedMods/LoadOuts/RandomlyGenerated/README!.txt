c950ff7902aef326f93222ab8f42135c5cd44eb3
DaOne <markkoky44@gmail.com>
2019-02-14 18:53:32 +1030

    DZM.Kill( true );
    RIP DZM

--- mpmissions/DayZSurvival.chernarusplus/ScriptedMods/LoadOuts/RandomlyGenerated/README!.txt
+++
@@ -1,2 +0,0 @@
-Add any item you wish to the correct catagory so it will spawn on the player randomly.
-check ModSettings.c for more info
--- mpmissions/DayZSurvival.chernarusplus/ScriptedMods/Modules/Misc/MOTDMessages.c
+++
@@ -1,8 +0,0 @@
-int TIME_BT_EACH_MSG = 60000; //in ms
-int TIME_INTERVAL = TIME_BT_EACH_MSG + Math.RandomIntInclusive(5000,15000); //5 sec - 15 sec Random addition to TIME_BT_EACH_MSG
-
-void CustomMOTD()
-{
-   TStringArray Messages = {"Welcome to My Server!","DayZ Modded Survival By DaOne"};
-   GetGame().ChatPlayer(5,Messages.GetRandomElement());
-}
